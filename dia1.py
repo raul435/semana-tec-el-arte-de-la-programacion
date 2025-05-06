986e6293bfd3714dbee9846da58fc15135baa06d
raul435 <105726590+raul435@users.noreply.github.com>
2025-05-06 07:41:12 -0600

    Update dia1.py

--- dia1.py
+++ dia1.py
@@ -2,7 +2,7 @@ import cv2
 import pytesseract
 import numpy as np
 
-imagen = cv2.imread('placa_4.jpg')  # Reemplaza con tu archivo
+imagen = cv2.imread('placa_4.jpg') 
 
 gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
 filtro = cv2.bilateralFilter(gris, 11, 17, 17)
@@ -22,11 +22,11 @@ for c in contornos:
         roi = gris[y:y + h, x:x + w]
         break
 
-texto = pytesseract.image_to_string(roi, config='--psm 8')  # Ajustar según resultado
+texto = pytesseract.image_to_string(roi, config='--psm 8') 
 print("Texto detectado en la placa:", texto.strip())
 cv2.drawContours(imagen, [placa], -1, (0, 255, 0), 3)
 
-cv2_imshow(imagen) # Use cv2_imshow instead of cv2.imshow
-cv2_imshow(roi if placa is not None else gris) # Use cv2_imshow instead of cv2.imshow
+cv2_imshow(imagen) 
+cv2_imshow(roi if placa is not None else gris) 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
